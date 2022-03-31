62d527e4ad08bb5377ca7d630399098c51a2cbea
JeongHwan96 <76175940+JeongHwan96@users.noreply.github.com>
2022-03-31 15:11:43 +0900

    remove basename

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Home from "./page/Home";
 
 function App() {
   return (
-    <BrowserRouter basename="JeongKimCook">
+    <BrowserRouter>
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
